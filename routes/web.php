39931acd34fd471f8c41ba8b838bf3c8064f40fe
dennis <1016144734grc@gmail.com>
2017-11-30 15:04:54 +0800

    'seed'

--- routes/web.php
+++ routes/web.php
@@ -37,3 +37,5 @@ Route::resource('/users', 'UsersController')->only(['show', 'update', 'edit']);
 
 
 
+
+Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
